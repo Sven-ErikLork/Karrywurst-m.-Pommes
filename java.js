82913df89857f7925dcd04c68fc3d612845a5001
jeffovitch <43370523+jeffovitch@users.noreply.github.com>
2019-05-30 10:55:22 +0200

    Add files via upload
    Rettet karuseltiden på slides

--- java.js
+++ java.js
@@ -10,7 +10,7 @@ for (i = 0; i < x.length; i++) {
     myIndex++;
 if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-setTimeout(carousel, 2000); // Change image every 2 seconds
+setTimeout(carousel, 5000); // Change image every 5 seconds
     }
 
 $(document).ready(function(){
